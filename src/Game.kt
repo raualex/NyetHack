b54d0a89b3277b5d40bbcf998d499841b6919831
Alexander Rau <alexanderrau86@yahoo.com>
2019-11-04 14:22:04 -0700

    Finish exercises for Chapter 4

--- src/Game.kt
+++ src/Game.kt
@@ -5,24 +5,48 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     //Aura
-    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
     //Health Status
-    val healthStatus = when (healthPoints) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    //Player Status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball()
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
-                "has some minor wounds but is healing quite quickly!"
-            } else {
-                "has some minor wounds."
-            }
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
 
-    //Player Status
-    println("(Aura: $auraColor)" +
-        "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor)" +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
     println("$name $healthStatus")
 }
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
