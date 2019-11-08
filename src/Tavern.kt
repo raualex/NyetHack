96ca70d4eddfd487cab21e1bf6f8ae8ae6dbadae
Alexander Rau <alexanderrau86@yahoo.com>
2019-11-08 16:29:57 -0700

    Finish Chapter 6

---
+++ src/Tavern.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var beverage = readLine()
+//    beverage = null
+
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
+
+//    VVV functionally the same as above VVV
+
+//    beverage?.let {
+//        beverage = it.capitalize()
+//    } ?: println("I can't do that without crashing - beverage was null!")
+
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
+}
