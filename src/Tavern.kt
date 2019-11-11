370195eef055ab88df5d1824275ddf193a37f2a7
Alexander Rau <alexanderrau86@yahoo.com>
2019-11-11 14:25:20 -0700

    Finish chapter 7

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,19 +1,35 @@
-fun main(args: Array<String>) {
-    var beverage = readLine()
-//    beverage = null
+const val TAVERN_NAME = "Tearnyl's Folly"
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that without crashing - beverage was null!")
+fun main(args: Array<String>) {
+    placeOrder("shandy,DRAGON'S BREATH,5.91")
+//    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value) {
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|_|"
+            else -> it.value
+        }
     }
 
-//    VVV functionally the same as above VVV
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
 
-//    beverage?.let {
-//        beverage = it.capitalize()
-//    } ?: println("I can't do that without crashing - beverage was null!")
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
 
-    val beverageServed: String = beverage ?: "Buttered Ale"
-    println(beverageServed)
+    val phrase = if (name == "Dragon's Breath" || name == "DRAGON'S BREATH") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
 }
