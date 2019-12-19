cbae7873e1d52e01a76d953b1dc36210bd9a07a4
Alexander Rau <alexanderrau86@yahoo.com>
2019-12-18 18:03:02 -0700

    Start Chapter 12 modifications and exercises

--- src/Game.kt
+++ src/Game.kt
@@ -1,52 +1,21 @@
 fun main(args: Array<String>) {
-    val name = "Madrigal"
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
+    val player = Player()
+    player.castFireball()
 
     //Aura
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    val auraColor = player.auraColor()
 
     //Health Status
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    val healthStatus = player.formatHealthStatus()
 
     //Player Status
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-
-    castFireball()
+    printPlayerStatus(player)
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
+private fun printPlayerStatus(player: Player) {
     println(
-        "(Aura: $auraColor)" +
-                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+        "(Aura: ${player.auraColor()})" +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
     )
-    println("$name $healthStatus")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
-
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
-
-private fun castFireball(numFireballs: Int = 2) =
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
