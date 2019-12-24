748160feab174fafaf49d8523f32f50774716a4e
Alexander Rau <alexanderrau86@yahoo.com>
2019-12-24 12:39:07 -0700

    Finish Chapter 15

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.nyethack
 
+import java.lang.IllegalStateException
+
 fun main(args: Array<String>) {
     Game.play()
 }
@@ -7,6 +9,10 @@ fun main(args: Array<String>) {
 object Game {
     private val player = Player("Madrigal")
     private var currentRoom: Room = TownSquare()
+
+    private var worldMap = listOf(
+        listOf(currentRoom, Room("Tavern"), Room("Back Room")),
+        listOf(Room("Long Corridor"), Room("Generic Room")))
 
     init {
         println("Welcome, adventurer.")
@@ -35,12 +41,29 @@ object Game {
     private class GameInput(arg: String?) {
         private val input = arg ?: ""
         val command = input.split(" ")[0]
-        val arguement = input.split(" ").getOrElse(1, { "" })
+        val argument = input.split(" ").getOrElse(1, { "" })
 
         fun processCommand() = when (command.toLowerCase()) {
+            "move" -> move(argument)
             else -> commandNotFound()
         }
 
         private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
     }
+
+    private fun move(directionInput: String) =
+            try {
+                val direction = Direction.valueOf(directionInput.toUpperCase())
+                val newPosition = direction.updateCoordinate(player.currentPosition)
+                if(!newPosition.isInBounds) {
+                    throw IllegalStateException("$direction is out of bounds.")
+                }
+
+                val newRoom = worldMap[newPosition.y][newPosition.x]
+                player.currentPosition = newPosition
+                currentRoom = newRoom
+                "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+            } catch (e: Exception) {
+                "Invalid direction: $directionInput."
+            }
 }
