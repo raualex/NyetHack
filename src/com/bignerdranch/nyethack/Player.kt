577981c71b79171dbc358517cbb593e2e5cc5bfd
Alexander Rau <alexanderrau86@yahoo.com>
2019-12-27 12:15:04 -0700

    Finish Chapter 18

--- src/com/bignerdranch/nyethack/Player.kt
+++ src/com/bignerdranch/nyethack/Player.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.nyethack
 
 import java.io.File
+import com.bignerdranch.nyethack.extensions.random as randomizer
 
 class Player(_name: String,
     override var healthPoints: Int = 100,
@@ -67,6 +68,5 @@ class Player(_name: String,
     private fun selectHometown() = File("data/towns.txt")
         .readText()
         .split("\n")
-        .shuffled()
-        .first()
+        .randomizer()
 }
