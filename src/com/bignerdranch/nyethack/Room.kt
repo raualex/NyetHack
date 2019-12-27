16257f971a07235f123416f23159352a77c31dcd
Alexander Rau <alexanderrau86@yahoo.com>
2019-12-27 13:36:53 -0700

    Finish Chapter 18 extensions

--- src/com/bignerdranch/nyethack/Room.kt
+++ src/com/bignerdranch/nyethack/Room.kt
@@ -1,4 +1,5 @@
 package com.bignerdranch.nyethack
+import com.bignerdranch.nyethack.extensions.frame
 
 //fun Room.configurePitGoblin(block: Room.(Goblin) -> Goblin): Room {
 //    val goblin = block(Goblin("Pit Goblin", description = "An Evil Pit Goblin"))
@@ -14,7 +15,7 @@ open class Room(val name: String) {
             "Danger level: $dangerLevel\n" +
             "Creature: ${monster?.description ?: "none."}"
 
-    open fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here...".frame(5)
 }
 
 open class TownSquare : Room("Town Square") {
@@ -22,7 +23,7 @@ open class TownSquare : Room("Town Square") {
     private var bellSound = "GWONG"
 
     final override fun load() =
-        "The villagers rally and cheer as you enter!\n${ringBell()} "
+        "The villagers rally and cheer as you enter!".frame(5) + "\n" + "${ringBell()}".frame(5)
 
     private fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
--- src/com/bignerdranch/nyethack/extensions/IterableExt.kt
+++ src/com/bignerdranch/nyethack/extensions/IterableExt.kt
@@ -1,3 +1,12 @@
 package com.bignerdranch.nyethack.extensions
 
 fun <T> Iterable<T>.random(): T = this.shuffled().first()
+
+fun String.frame(padding: Int, formatChar: String = "*"): String {
+    val greeting = this
+    val middle = formatChar.padEnd(padding)
+        .plus(greeting)
+        .plus(formatChar.padStart(padding))
+    val end = (0 until middle.length).joinToString("") { formatChar }
+    return "$end\n$middle\n$end"
+}
